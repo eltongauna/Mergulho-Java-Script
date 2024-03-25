d5d8a6f335e08feb14c00d15dcca24eb0e1fc1bb
Elton Gaúna Galera <eltongaunagalera@gmail.com>
2024-03-25 11:56:56 -0300

    Demonstração de como criar objetos e suas manipulações de alteração e exibição.

---
+++ 02-03-objetos/script.js
@@ -0,0 +1,14 @@
+// exemplo de como definir um objeto
+var pessoa = {
+    nome: 'Elton',
+    idade: 40,
+    sexo: "masculino"
+}
+
+//como exibir um valor de um atributo específico
+console.log(pessoa.nome)
+
+//como alterar um atributo
+pessoa.idade = 41
+
+console.log(pessoa)
