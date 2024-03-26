d43821c8feb3ebe640b162c0a2422561ef4f95d1
Elton Gaúna Galera <eltongaunagalera@gmail.com>
2024-03-25 21:54:14 -0300

    Estudo sobre Truthy e Falsy

---
+++ 02-08-truthy-falsy/index.js
@@ -0,0 +1,23 @@
+/*
+Truthy
+true
+[]
+{}
+42
+new Date()
+-42
+3.14
+-3.14
+Infinity
+-Infinity
+********************************
+
+Falsy
+false
+0
+null
+undefined
+NaN
+"" ou ''
+document.all
+*/
